29abd7637f26946d2a44168beaeb0d75cb85778d
Willy <ai791016ai791016@gmail.com>
2020-03-01 11:24:26 +0800

    Build for production

---
+++ build/precache-manifest.26456e20ecc6e2ca20bcb9023433d548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c36da7190a4324dec6fdaebd0e89c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d2176de2c9871b8c735",
+    "url": "/static/css/main.a1751158.chunk.css"
+  },
+  {
+    "revision": "1e387cba2d639d9430ce",
+    "url": "/static/js/2.f9780263.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/static/js/2.f9780263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2176de2c9871b8c735",
+    "url": "/static/js/main.a56eaaa0.chunk.js"
+  },
+  {
+    "revision": "4d58ec16be8846f64c5e",
+    "url": "/static/js/runtime-main.53e61133.js"
+  }
+]);
